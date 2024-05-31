3cdec735c24b66e9cc1af6372d1c736c3ea755ca
Nicholas2k4 <nicholas.s2k4@gmail.com>
2024-05-31 22:02:20 +0700

    Fitur jual

---
+++ database/migrations/2024_05_30_160910_create_nota_juals_table.php
@@ -0,0 +1,33 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('nota_juals', function (Blueprint $table) {
+            $table->id();
+            $table->string('nama');
+            $table->string('foto');
+            $table->string('status')->default(0); //0 = belum dikonfimasi, 1 = approved, 2 = rejected
+            $table->float('harga');
+            $table->foreignId('customer_id')->constrained()->cascadeOnDelete();
+            $table->foreignId('wirausaha_id')->nullable()->constrained()->cascadeOnDelete();
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('nota_juals');
+    }
+};
