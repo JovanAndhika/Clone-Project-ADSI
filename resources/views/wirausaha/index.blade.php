cdbb3d6098e6518063eeb32e97dd75e4bd785a00
M4RV3L24 <marvelodelio@gmail.com>
2024-06-03 01:20:17 +0700

    update dashboard wirausaha

--- resources/views/wirausaha/index.blade.php
+++ resources/views/wirausaha/index.blade.php
@@ -1,12 +1,16 @@
 @extends('layouts.wirausaha')
 
 @section('content')
-    <h1 class="mx-auto text-center my-3 text-uppercase fw-bold">WELCOME</h1>
-    @include('components.alert')
-    <div class="shadow-lg rounded col-7 mx-auto d-flex justify-content-center flex-col">
-        <a href="{{ @route('wirausaha.barang') }}" class="btn btn-dark w-100 m-2">Manage Barang</a>
-        <a href="{{ @route('wirausaha.offer') }}" class="btn btn-dark w-100 m-2">Manage Offer</a>
-        
+    <div class="d-flex align-items-center justify-content-center" style="min-height:100vh">
+        <div class="col-7 rounded bg-light py-2 bg-opacity-50">
+
+            <h1 class="mx-auto text-center my-3 text-uppercase fw-bold">WELCOME</h1>
+            @include('components.alert')
+            <div class="shadow-lg rounded mx-auto text-center">
+                <a href="{{ @route('wirausaha.barang') }}" class="btn btn-dark w-75 text-center p-2 my-2 d-inline-block">Manage Barang</a>
+                <a href="{{ @route('wirausaha.offer') }}" class="btn btn-dark w-75 text-center p-2 my-2 d-inline-block">Manage Offer</a>
+
+            </div>
+        </div>
     </div>
 @endsection
-
