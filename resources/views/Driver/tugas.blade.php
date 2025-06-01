47381177ffc1b22ace639cda9f8162ecc9ecb1cc
Jovan Andhika <c14220013@john.petra.ac.id>
2025-06-01 17:18:44 +0700

    create: driver layouts

--- resources/views/Driver/tugas.blade.php
+++ resources/views/Driver/tugas.blade.php
@@ -1,19 +1,6 @@
-<!DOCTYPE html>
-<html lang="en">
+@extends('layouts.driver')
 
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <!-- Bootstrap Icon -->
-    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.11.3/font/bootstrap-icons.min.css">
-    <!-- Bootstrap 5 -->
-    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
-    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-YvpcrYf0tY3lHB60NNkmXc5s9fDVZLESaAA55NDzOxhy9GkcIdslK1eN7N6jIeHz" crossorigin="anonymous"></script>
-    <title>Driver | Tugas</title>
-</head>
-
-<body>
-    @include('driver.components.sidebar')
+@section('content')
     <!-- Tugas Belum Diambil -->
     <section>
         <style>
@@ -586,6 +573,4 @@
     </div>
     @endforeach
     <!-- END OF MODAL -->
-</body>
-
-</html>
+@endsection
