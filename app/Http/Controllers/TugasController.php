ecbba6f01f75b6b4a5780ba910f5c9d36baf28ee
Jovan Andhika <c14220013@john.petra.ac.id>
2025-06-01 18:05:51 +0700

    add: try catch driver index

--- app/Http/Controllers/TugasController.php
+++ app/Http/Controllers/TugasController.php
@@ -14,44 +14,54 @@ class TugasController extends Controller
         // login as driver
         auth()->guard('driver')->loginUsingId(1);
 
-        //FUNCTION UNTUK AMBIL MODEL
-        // 1.Tugas delivery/pengantaran menuju customer
-        $list_tugas_beli_berlangsung = Tugas::where('status', 'berlangsung')
-            ->whereHas('nota_beli', function ($query) {
-            $query->where('status', 0);
-            })
-            ->with('nota_beli.barang')
-            ->get();
 
-        $list_tugas_beli = Tugas::where('status', 'belum_diambil')
-            ->whereHas('nota_beli', function ($query) {
-            $query->where('status', 0);
-            })
-            ->with('nota_beli.barang')
-            ->get();
+        try {
+            // proses logic
+
+            //FUNCTION UNTUK AMBIL MODEL
+            // 1.Tugas delivery/pengantaran menuju customer
+            $list_tugas_beli_berlangsung = Tugas::where('status', 'berlangsung')
+                ->whereHas('nota_beli', function ($query) {
+                    $query->where('status', 0);
+                })
+                ->with('nota_beli.barang')
+                ->get();
+
+            $list_tugas_beli = Tugas::where('status', 'belum_diambil')
+                ->whereHas('nota_beli', function ($query) {
+                    $query->where('status', 0);
+                })
+                ->with('nota_beli.barang')
+                ->get();
 
 
-        // 2. Tugas take/penjemputan barang menuju wirausaha
-        $list_tugas_jual_berlangsung = Tugas::where('status', 'berlangsung')
-            ->whereHas('nota_jual', function ($query) {
-                $query->where('status', 1);
-            })
-            ->with('nota_jual')
-            ->get();
-            
-        $list_tugas_jual = Tugas::where('status', 'belum_diambil')
-            ->whereHas('nota_jual', function ($query) {
-                $query->where('status', 1);
-            })
-            ->with('nota_jual')
-            ->get();
+            // 2. Tugas take/penjemputan barang menuju wirausaha
+            $list_tugas_jual_berlangsung = Tugas::where('status', 'berlangsung')
+                ->whereHas('nota_jual', function ($query) {
+                    $query->where('status', 1);
+                })
+                ->with('nota_jual')
+                ->get();
 
-        return view('driver.tugas', [
-            'list_tugas_beli_berlangsung' => $list_tugas_beli_berlangsung,
-            'list_tugas_beli' => $list_tugas_beli,
-            'list_tugas_jual_berlangsung' => $list_tugas_jual_berlangsung,
-            'list_tugas_jual' => $list_tugas_jual,
-        ]);
+            $list_tugas_jual = Tugas::where('status', 'belum_diambil')
+                ->whereHas('nota_jual', function ($query) {
+                    $query->where('status', 1);
+                })
+                ->with('nota_jual')
+                ->get();
+
+            return view('driver.tugas', [
+                'list_tugas_beli_berlangsung' => $list_tugas_beli_berlangsung,
+                'list_tugas_beli' => $list_tugas_beli,
+                'list_tugas_jual_berlangsung' => $list_tugas_jual_berlangsung,
+                'list_tugas_jual' => $list_tugas_jual,
+            ]);
+        } catch (\Throwable $e) {
+            return response()->json([
+                'error' => $e->getMessage(),
+                'trace' => $e->getTrace()
+            ], 500);
+        }
     }
 
     public function ambilTugas($idTugas)
